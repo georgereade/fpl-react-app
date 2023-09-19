36be0a4abe295fc658b8fad7b3b23ece8a5106e5
georgereade <georgereade@hotmail.co.uk>
2023-09-19 12:02:23 +0100

    Typo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,6 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import Offcanvas from "react-bootstrap/Offcanvas";
 
 function Header() {
   return (
